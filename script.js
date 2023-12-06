1b8452ec2d2f496747e07cc3b5d7c95723713737
ctih <nevalaonni@proton.me>
2023-12-06 20:23:05 +0200

    Added something, I forgot what

---
+++ script.js
@@ -0,0 +1,11 @@
+function DC() {
+    window.open("https://discord.com/invite/Sn2zJJ25", "_blank");
+}
+
+function TryIt() {
+    window.open("https://www.example.com/", "_blank");
+}
+
+function PlaceholderButton() {
+    window.open("https://www.example.com/", "_blank");
+}
