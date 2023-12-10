3841bc6b20c88b4c148f508c35d4ec02a184d18c
ctih <nevalaonni@proton.me>
2023-12-10 17:05:21 +0200

    added smooth scrolling animation

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function PlaceholderButton() {
     window.open("https://www.example.com/", "_blank");
 }
 
+const container = document.querySelector('.scroll-container');
+const items = document.querySelectorAll('.scroll-item');
+
+container.addEventListener('wheel', (event) => {
+  event.preventDefault();
+  const delta = event.deltaY;
+
+  container.scrollBy({
+    top: delta,
+    behavior: 'smooth'
+  });
+}, {passive: false});
