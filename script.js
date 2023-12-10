0405e4646b72d1b597b0be5359d11437f4952be3
ctih <nevalaonni@proton.me>
2023-12-10 20:51:16 +0200

    added other elements

--- script.js
+++ script.js
@@ -10,16 +10,15 @@ function PlaceholderButton() {
     window.open("https://www.example.com/", "_blank");
 }
 
+
 const container = document.querySelector('.scroll-container');
-let isScrolling = false;
-let startPos = 0;
-let currentPos = 0;
 
 container.addEventListener('wheel', (event) => {
   event.preventDefault();
   
   const delta = event.deltaY || event.detail || event.wheelDelta;
-  
+  console.log(delta);
+  console.log(event.deltaY, event.detail, event.wheelDelta );
   container.scrollBy({
     top: delta,
     behavior: 'smooth'
