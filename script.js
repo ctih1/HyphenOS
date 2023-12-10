8f6b56d83c3a6eb4d3bbba397b73aa3da8ebed4c
ctih <nevalaonni@proton.me>
2023-12-10 17:11:49 +0200

    MAybe it works on mobile?

--- script.js
+++ script.js
@@ -22,3 +22,13 @@ container.addEventListener('wheel', (event) => {
     behavior: 'smooth'
   });
 }, {passive: false});
+
+container.addEventListener('touchstart', (event) => {
+    event.preventDefault();
+    const delta = event.deltaY;
+  
+    container.scrollBy({
+      top: delta,
+      behavior: 'smooth'
+    });
+  }, {passive: false});
